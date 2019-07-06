a8e7ab47dff97274d4928341671d20dcff39a95b
Owen Nelson <onelson@gmail.com>
2019-07-06 14:42:05 -0700

    update example wrt #6

--- examples/simple-cli.rs
+++ examples/simple-cli.rs
@@ -4,10 +4,10 @@ use std::env;
 fn main() {
     let mut args = env::args().skip(1);
 
-    let program = args.next().unwrap();
-    let input = args.next().unwrap();
+    let program = args.next().expect("jq program");
+    let input = args.next().expect("data input");
     match jq_rs::run(&program, &input) {
-        Ok(s) => println!("{}", s),
+        Ok(s) => print!("{}", s), // The output will include a trailing newline
         Err(e) => eprintln!("{}", e),
     }
 }
